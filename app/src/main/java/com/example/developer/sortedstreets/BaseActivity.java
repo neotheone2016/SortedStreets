fd9f1ef0604c846e452feb719da4560b00c95b4b
neotheone2016 <shishanksingh2014@gmail.com>
2016-04-24 12:56:54 +0530

    created base activity

--- app/src/main/java/com/example/developer/sortedstreets/BaseActivity.java
+++ app/src/main/java/com/example/developer/sortedstreets/BaseActivity.java
@@ -1,6 +1,40 @@
 package com.example.developer.sortedstreets;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class BaseActivity extends AppCompatActivity {
+    private FrameLayout mContentFrame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_base);
+
+        mContentFrame = (FrameLayout) findViewById(R.id.frame_container);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        getLayoutInflater().inflate(layoutResID, mContentFrame, true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
 }
